66af664056126c8a35d7e384b1df722d8ce0aa70
Vovik <volodya-12347@rambler.ru>
2021-06-26 23:26:02 +0300

    fixed settings listing

--- 1.2/Source/Rainbeau's Fishing/Mod/Settings.cs
+++ 1.2/Source/Rainbeau's Fishing/Mod/Settings.cs
@@ -5,11 +5,9 @@ namespace RBB
 {
     public class Settings : ModSettings
     {
-        public float FishingEfficiency => fishingEfficiency / 100;
-        public float TrapEfficiency => trapEfficiency / 100;
 
-        public float fishingEfficiency = 100.0f;
-        public float trapEfficiency = 100.0f;
+        public float FishingEfficiency = 1.0f;
+        public float TrapEfficiency = 1.0f;
         
         public float failureLevel = 1.5f;
         public float junkLevel = 1.5f;
@@ -24,27 +22,35 @@ namespace RBB
         public float junkChance = 0.25f;
         public float roeChance = 0.5f;
 
+        private static Vector2 scrollVector2 = new Vector2(0, 0);
+
         public void DoWindowContents(Rect canvas)
         {
-            Listing_Standard list = new Listing_Standard();
-            list.ColumnWidth = canvas.width;
-            list.Begin(canvas);
+            float height = (12f + 22f + 2 * Text.CalcHeight("test", canvas.width)) * 12;
 
-            list.Gap();
-            list.Label(ResourceBank.Strings.FishingEfficiency.Translate() + "  " + (int)fishingEfficiency + "%");
-            fishingEfficiency = list.Slider(fishingEfficiency, 50f, 150.99f);
+            Rect outRect = new Rect(canvas.x, canvas.y, canvas.width - 20, canvas.height);
+            Rect viewRect = new Rect(0, 0, canvas.width - 20, height);
+
+            Listing_Standard list = new Listing_Standard();
+            list.ColumnWidth = canvas.width - 40;
+            list.Begin(viewRect);
+
+            Widgets.BeginScrollView(outRect, ref scrollVector2, viewRect);
+
+            list.Label(ResourceBank.Strings.FishingEfficiency.Translate() + "  " + FishingEfficiency.ToString("p"));
+            FishingEfficiency = list.Slider(FishingEfficiency, 0.5f, 1.5f);
             Text.Font = GameFont.Tiny;
             list.Label("          " + ResourceBank.Strings.FishingEfficiencyTip.Translate());
             Text.Font = GameFont.Small;
 
             list.Gap();
-            list.Label(ResourceBank.Strings.SettingTrapEfficiency.Translate() + "  " + (int)trapEfficiency + "%");
-            trapEfficiency = list.Slider(trapEfficiency, 50f, 150.99f);
+            list.Label(ResourceBank.Strings.SettingTrapEfficiency.Translate() + "  " + TrapEfficiency.ToString("p"));
+            TrapEfficiency = list.Slider(TrapEfficiency, 0.5f, 1.5f);
             Text.Font = GameFont.Tiny;
             list.Label("          " + ResourceBank.Strings.SettingTrapEfficiencyTip.Translate());
             Text.Font = GameFont.Small;
-            list.Gap();
 
+            list.Gap();
             list.Label(ResourceBank.Strings.FailureLevel.Translate(failureLevel));
             failureLevel = list.Slider(failureLevel, 0, 4);
             Text.Font = GameFont.Tiny;
@@ -58,6 +64,7 @@ namespace RBB
             else if (failureLevel < 3) { list.Label("          " + ResourceBank.Strings.SettingNoticeDing.Translate()); }
             else { list.Label("          " + ResourceBank.Strings.SettingNoticeBox.Translate()); }
             Text.Font = GameFont.Small;
+
             list.Gap();
             list.Label(ResourceBank.Strings.JunkLevel.Translate(junkLevel));
             junkLevel = list.Slider(junkLevel, 0, 4);
@@ -72,6 +79,7 @@ namespace RBB
             else if (junkLevel < 3) { list.Label("          " + ResourceBank.Strings.SettingNoticeDing.Translate()); }
             else { list.Label("          " + ResourceBank.Strings.SettingNoticeBox.Translate()); }
             Text.Font = GameFont.Small;
+
             list.Gap();
             list.Label(ResourceBank.Strings.SuccessLevel.Translate(SuccessLevel));
             SuccessLevel = list.Slider(SuccessLevel, 0, 4);
@@ -86,6 +94,7 @@ namespace RBB
             else if (SuccessLevel < 3) { list.Label("          " + ResourceBank.Strings.SettingNoticeDing.Translate()); }
             else { list.Label("          " + ResourceBank.Strings.SettingNoticeBox.Translate()); }
             Text.Font = GameFont.Small;
+
             list.Gap();
             list.Label(ResourceBank.Strings.TreasureLevel.Translate(treasureLevel));
             treasureLevel = list.Slider(treasureLevel, 0, 4);
@@ -116,41 +125,42 @@ namespace RBB
             Text.Font = GameFont.Small;
 
             list.Gap();
-            list.Label(ResourceBank.Strings.TreasureChance.Translate() + "  " + treasureChanceStandart.ToString("0.00") + "%");
+            list.Label(ResourceBank.Strings.TreasureChance.Translate() + "  " + treasureChanceStandart.ToString("p"));
             treasureChanceStandart = list.Slider(treasureChanceStandart, 0.00001f, 0.001f);
             Text.Font = GameFont.Tiny;
             list.Label("          " + ResourceBank.Strings.TreasureChanceTip.Translate());
             Text.Font = GameFont.Small;
 
             list.Gap();
-            list.Label(ResourceBank.Strings.TreasureMaxValue.Translate() + "  " + treasureMaxValue.ToString("0.00") + "%");
+            list.Label(ResourceBank.Strings.TreasureMaxValue.Translate() + "  " + treasureMaxValue.ToString("0.0"));
             treasureMaxValue = list.Slider(treasureMaxValue, 100f, 11000f);
             Text.Font = GameFont.Tiny;
             list.Label("          " + ResourceBank.Strings.TreasureMaxValueTip.Translate());
             Text.Font = GameFont.Small;
             
             list.Gap();
-            list.Label(ResourceBank.Strings.JunkChance.Translate() + "  " + junkChance.ToString("0.00") + "%");
+            list.Label(ResourceBank.Strings.JunkChance.Translate() + "  " + junkChance.ToString("p"));
             junkChance = list.Slider(junkChance, 0f, 1f);
             Text.Font = GameFont.Tiny;
             list.Label("          " + ResourceBank.Strings.JunkChanceTip.Translate());
             Text.Font = GameFont.Small;
 
             list.Gap();
-            list.Label(ResourceBank.Strings.RoeChance.Translate() + "  " + roeChance.ToString("0.00") + "%");
+            list.Label(ResourceBank.Strings.RoeChance.Translate() + "  " + roeChance.ToString("p"));
             roeChance = list.Slider(roeChance, 0f, 1f);
             Text.Font = GameFont.Tiny;
             list.Label("          " + ResourceBank.Strings.RoeChanceTip.Translate());
             Text.Font = GameFont.Small;
 
+            Widgets.EndScrollView();
             list.End();
         }
 
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Values.Look(ref fishingEfficiency, "fishingEfficiency", 100.0f);
-            Scribe_Values.Look(ref trapEfficiency, "trapEfficiency", 100.0f);
+            Scribe_Values.Look(ref FishingEfficiency, "fishingEfficiency", 100.0f);
+            Scribe_Values.Look(ref TrapEfficiency, "trapEfficiency", 100.0f);
             Scribe_Values.Look(ref failureLevel, "failureLevel", 1.5f);
             Scribe_Values.Look(ref junkLevel, "junkLevel", 1.5f);
             Scribe_Values.Look(ref SuccessLevel, "successLevel", 1.5f);
